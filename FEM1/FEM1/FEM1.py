1b16486a4a1f3643fff1f7cf0bff0add70930389
Arex710 <stechlyarkadiusz@gmail.com>
2021-04-08 13:23:26 +0200

    Wykonana instrukcja Numpy i matplotlib

--- FEM1/FEM1/FEM1.py
+++ FEM1/FEM1/FEM1.py
@@ -171,9 +171,52 @@ import matplotlib.pyplot as plt
 
 #5.1 Wykresy funkcji
 
-x = [1,2,3]
-y = [4,6,5]
-plt.plot(x,y)
-plt.show()
+#x = [1,2,3]
+#y = [4,6,5]
+#plt.plot(x,y)
+#plt.show()
+
+#5.1.1 Rysujemy wykres funkcji sinus
+#x = np.arange(0.0, 2.0, 0.01)
+#y = np.sin(2.0*np.pi*x)
+#plt.plot(x,y)
+#plt.show()
+
+#5.1.2 Ulepszamy wykres
+#x = np.arange(0.0, 2.0, 0.01)
+#y = np.sin(2.0*np.pi*x)
+#plt.plot(x,y,'r:',linewidth=6)
+#plt.xlabel('Czas')
+#plt.ylabel('Pozycja')
+#plt.title('Nasz pierwszy wykres')
+#plt.grid(True)
+#plt.show()
+
+#5.1.3 Kilka wykresów we wspólnych osiach - Pierwsza wersja
+#x = np.arange(0.0, 2.0, 0.01)
+#y1 = np.sin(2.0*np.pi*x)
+#y2 = np.cos(2.0*np.pi*x)
+#plt.plot(x,y1,'r:',x,y2,'g')
+#plt.legend(('dane y1','dane y2'))
+#plt.xlabel('Czas')
+#plt.ylabel('Pozycja')
+#plt.title('Wykres')
+#plt.grid(True)
+#plt.show()
+
+#5.1.4 Kilka wykresów we wspólnych osiach - Druga wersja
+#x = np.arange(0.0, 2.0, 0.01)
+#y1 = np.sin(2.0*np.pi*x)
+#y2 = np.cos(2.0*np.pi*x)
+#y = y1*y2
+#l1, = plt.plot(x,y,'b')
+#l2,l3 = plt.plot(x,y1,'r:',x,y2,'g')
+#plt.legend((l2,l3,l1),('dane y1','dane y2','y1*y2'))
+#plt.xlabel('Czas')
+#plt.ylabel('Pozycja')
+#plt.title('Wykres')
+#plt.grid(True)
+#plt.show()
+
 
 # 6 Ćwiczenia
